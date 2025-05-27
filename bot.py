36c799a87ca5c1fa2907c434e12f4f79c82618c3
zolotykh907 <i.zolotykh@g.nsu.ru>
2025-05-27 11:53:22 +0700

    create files

---
+++ bot.py
@@ -0,0 +1,61 @@
+from typing import TypedDict
+from datetime import datetime, timezone
+from langchain_community.llms import Ollama
+from langgraph.graph import StateGraph
+
+
+State = TypedDict("State", {"user_input": str, "output": str})
+
+
+def get_current_time() -> dict:
+    """Return the current UTC time in ISO‑8601 format.
+    Example → {"utc": "2025‑05‑21T06:42:00Z"}"""
+
+    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    return {"utc": current_time}
+
+def is_time(input_text: str, llm: Ollama) -> bool:
+    """Проверим, содержит ли текст слова, связанные со временем, быстрая проверка"""
+    time_words = ["time now", "current time", "который час", "сколько времени"]
+    lower_input = input_text.lower()
+
+    for time_word in time_words:  
+        if time_word in lower_input:
+            return True
+    
+    prompt = f"""Определи, просит ли пользователь назвать текущее время. 
+    Ответ должен содержать строго одно слово: "YES" или "NO".
+    Запрос: "{input_text}"
+    """
+    
+    """Получаем ответ от модели, так как вопрос 
+    не обязательно может содержать слова, связанные со временем.
+    Требует больше времени, так как вызывается LLM, но это дает большую гибкость"""
+    answer = llm.invoke(prompt).strip()
+    up_answer = answer.upper()  
+
+    if up_answer not in ["YES", "NO"]:              
+        raise ValueError("LLM answer not YES or NO")
+    
+    if up_answer == "NO":
+        return False
+    
+    return True
+
+def message_process(state: State) -> State:
+    llm = Ollama(model="llama3")
+    user_input = state["user_input"]
+
+    if is_time(user_input, llm):
+        current_time = get_current_time()["utc"]
+        return {"user_input": user_input, "output": current_time}
+    
+    return {"user_input": user_input, "output": llm.invoke(user_input)}
+
+
+app = (
+    StateGraph(State)
+    .add_node("process", message_process)
+    .add_edge("__start__", "process")
+    .compile()
+)
